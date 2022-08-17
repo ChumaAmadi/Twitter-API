3a0b171f875cf57770b522a3bab1496de18547ec
Chukwuma Nnodim-Amadi <C.NnodimAmadi001@umb.edu>
2022-08-17 15:28:04 -0400

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-
 import Navbar from "./Navbar";
 import TweetFeed from "./TweetFeed";
 import RuleList from "./RuleList";
